9509e495893448916b90212bfbfa856dd99852ad
unknown <johnnytansk@yahoo.com>
2018-01-18 23:46:26 +0800

    a

--- learning_forms/basic/admin.py
+++ learning_forms/basic/admin.py
@@ -1,7 +1,38 @@
 from django.contrib import admin
-from basic.models import Member, Order, Profile
+from django import forms
+from basic.models import (Member,
+        Order,
+        Profile,
+        Server,
+        IPPool,
+        Nic)
+
+
+class NicAdmin(admin.ModelAdmin):
+    readonly_fields = ('ip',)
+    list_display =  ('server', 'model', 'slot', 'ip')
+    fieldsets = (
+        (None, {
+            'fields': ('server', 'model', 'slot')
+        }),
+        ('Advanced options', {
+            'fields': ('ip',)
+        })
+    )
+
+
+class IPPoolAdmin(admin.ModelAdmin):
+    list_display =  ('server', 'ip', 'in_used')
+    fieldsets = (
+        (None, {
+            'fields': ('server', 'ip','in_used')
+        }),
+    )
 
 # Register your models here.
 admin.site.register(Member)
 admin.site.register(Order)
 admin.site.register(Profile)
+admin.site.register(Server)
+admin.site.register(IPPool,IPPoolAdmin)
+admin.site.register(Nic,NicAdmin)
--- learning_forms/basic/models.py
+++ learning_forms/basic/models.py
@@ -5,6 +5,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
 # Create your models here.
 class Member(models.Model):
     name = models.CharField(max_length=50,blank=False,unique=True)
@@ -61,3 +62,50 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profiles.save()
+
+
+class Server(models.Model):
+    name = models.CharField(max_length=20,blank=True)
+    data_center = models.CharField(max_length=100,blank=True)
+    ip = models.GenericIPAddressField(default='0.0.0.0', null=True, blank=True)
+    subnet = models.GenericIPAddressField(default='0.0.0.0', null=True, blank=True)
+    gateway = models.GenericIPAddressField(default='0.0.0.0', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class IPPool(models.Model):
+    server = models.ForeignKey(Server,on_delete=models.CASCADE, related_name='ips')
+    ip = models.GenericIPAddressField(default='0.0.0.0', null=True, blank=True)
+    in_used = models.BooleanField(blank=True, default=False)
+
+    def __str__(self):
+        return self.ip
+
+
+class Nic(models.Model):
+    server = models.ForeignKey(Server,on_delete=models.CASCADE, related_name='nics')
+    model = models.CharField(max_length=100,blank=True)
+    slot = models.SmallIntegerField(blank=True,null=True)
+    ip = models.GenericIPAddressField(default='0.0.0.0',
+                null=True,
+                blank=True)
+
+    def __str__(self):
+        return self.model
+
+
+@receiver(post_save, sender=Nic)
+def save_nic_setting(sender, instance, created, **kwargs):
+    is_assigned = False
+    if created:
+        ip_list = IPPool.objects.filter(server=instance.server.id)
+        for ippool in ip_list:
+            print(ippool.ip)
+            if not ippool.in_used:
+                instance.ip = ippool.ip
+                instance.save()
+                ippool.in_used = True
+                ippool.save()
+                is_assigned = False
+                break
